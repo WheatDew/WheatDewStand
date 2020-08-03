e195839d74e45cf8f5337ce02cf0255085779895
agefly <wheatdew@163.com>
2020-08-03 13:39:42 +0800

    添加项目

---
+++ Assets/Extra/Character/Systemes/NavMeshPathSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class NavMeshPathSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        MousePointMoveJob();
+    }
+
+    private void MousePointMoveJob()
+    {
+        if (Input.GetMouseButtonDown(1))
+            Entities.ForEach((CharacterStatus status, UnityEngine.AI.NavMeshAgent agent) =>
+            {
+                if (status.isConscriptSelected)
+                {
+                    RaycastHit hit;
+                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+                        agent.destination = hit.point;
+                }
+            });
+    }
+
+}
