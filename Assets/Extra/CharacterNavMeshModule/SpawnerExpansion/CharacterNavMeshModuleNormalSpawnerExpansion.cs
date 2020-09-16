79395ab82eab2024f5ffd92349fa70fcf6afd8e3
agefly <wheatdew@163.com>
2020-09-16 17:31:04 +0800

    人物寻路模块

---
+++ Assets/Extra/CharacterNavMeshModule/SpawnerExpansion/CharacterNavMeshModuleNormalSpawnerExpansion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterNavMeshModuleNormalSpawnerExpansion : MonoBehaviour
+{
+    private void Start()
+    {
+        GameObject CharacterBasicModulePrefab = FindObjectOfType<CharacterBasicModuleNormalSpawner>().CharacterBasicModulePrefab;
+        NavMeshAgent navMeshAgent= CharacterBasicModulePrefab.AddComponent<NavMeshAgent>();
+        navMeshAgent.baseOffset = 0;
+
+    }
+}
